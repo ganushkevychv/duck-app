45f65aa57f75ee310c6c1791d4270da2a5399a6e
ganushkevychv <ganushkevychv@gmail.com>
2021-07-19 20:14:02 +0300

    text added to info screen

--- App.tsx
+++ App.tsx
@@ -9,15 +9,25 @@ import { RenderProp } from '@ui-kitten/components/devsupport';
 import DuckImage from './src/components/Images/DuckImage';
 
 const DuckScreen = () => (
-  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20 }}>
     <Text category="h1">Talk to me</Text>
     <DuckImage/>
   </Layout>
 );
 
 const InfoScreen = () => (
-  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text category='h1'>Info goes here</Text>
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20 }}>
+    <Text category='h1'>Rubber Duck Debugging</Text>
+    <Layout style={{ maxWidth: 500 }}>
+      <Text category="p1">
+        Explain to the duck what your code is supposed to do,
+        and then go into detail and explain your code line by line.
+        At some point, you will tell the duck what you are doing next and
+        then realize that that is not in fact what you are actually doing.
+        The duck will sit there serenely, happy in the knowledge that it has helped
+        you on your way.
+      </Text>
+    </Layout>
   </Layout>
 );
 
