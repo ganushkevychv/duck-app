556ac68a037b5d94936d94b50849f0bfcdcc3407
ganushkevychv <ganushkevychv@gmail.com>
2021-07-19 10:58:39 +0300

    react nav installed, 2 dummy screens added

--- App.tsx
+++ App.tsx
@@ -4,11 +4,23 @@ import { StyleSheet } from 'react-native';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 
+const DuckScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text category='h1'>Duck goes here</Text>
+  </Layout>
+);
+
+const InfoScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text category='h1'>Info goes here</Text>
+  </Layout>
+);
+
 export default function App() {
   return (
     <ApplicationProvider {...eva} theme={eva.dark}>
       <Layout style={styles.container}>
-        <Text>I know what to change no</Text>
+        <Text>I know what to change now!)</Text>
         <StatusBar style="auto" />
       </Layout>
     </ApplicationProvider>
