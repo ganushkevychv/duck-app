c46ca8cc4cfbe1a829936210384376dae73168cf
ganushkevychv <ganushkevychv@gmail.com>
2021-07-19 11:06:53 +0300

    navigation components added

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { ApplicationProvider, Layout, Text, BottomNavigation, BottomNavigationTab } from '@ui-kitten/components';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator, BottomTabBarProps } from '@react-navigation/bottom-tabs';
 
 const DuckScreen = () => (
   <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -16,21 +16,30 @@ const InfoScreen = () => (
   </Layout>
 );
 
+const BottomTabBar: React.FC<BottomTabBarProps> = ({ navigation, state }) => (
+  <BottomNavigation
+    selectedIndex={state.index}
+    onSelect={index => navigation.navigate(state.routeNames[index])}>
+    <BottomNavigationTab title='DUCK'/>
+    <BottomNavigationTab title='INFO'/>
+  </BottomNavigation>
+);
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+const TabNavigator = () => (
+  <Navigator tabBar={props => <BottomTabBar {...props} />}>
+    <Screen name='Duck' component={DuckScreen}/>
+    <Screen name='Info' component={InfoScreen}/>
+  </Navigator>
+);
+
 export default function App() {
   return (
     <ApplicationProvider {...eva} theme={eva.dark}>
-      <Layout style={styles.container}>
-        <Text>I know what to change now!)</Text>
-        <StatusBar style="auto" />
-      </Layout>
+      <NavigationContainer>
+        <TabNavigator/>
+      </NavigationContainer>
     </ApplicationProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
