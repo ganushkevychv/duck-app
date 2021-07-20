e396a13cf261b7bb94686afcb6ab4accc7345bdb
ganushkevychv <ganushkevychv@gmail.com>
2021-07-20 10:08:47 +0300

    BirdIcon,InfoIcon  was removed from App.tsx and added as additional component

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, BottomNavigation, BottomNavigationTab, IconRegistry, Icon } from '@ui-kitten/components';
+import { ApplicationProvider, BottomNavigation, BottomNavigationTab, IconRegistry } from '@ui-kitten/components';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator, BottomTabBarProps } from '@react-navigation/bottom-tabs';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import { ImageProps } from 'react-native';
-import { RenderProp } from '@ui-kitten/components/devsupport';
 import DuckScreen from './src/screens/DuckScreen';
 import InfoScreen from './src/screens/InfoScreen';
-
-const BirdIcon: RenderProp<Partial<ImageProps>> = (props) => (
-  <Icon {...props} name='twitter-outline'/>
-);
-
-const InfoIcon: RenderProp<Partial<ImageProps>>  = (props) => (
-  <Icon {...props} name='info-outline'/>
-);
+import BirdIcon from './src/components/Icons/BirdIcon';
+import InfoIcon from './src/components/Icons/InfoIcon';
 
 const BottomTabBar: React.FC<BottomTabBarProps> = ({ navigation, state }) => (
   <BottomNavigation
