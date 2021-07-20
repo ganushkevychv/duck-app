96b2b2ee1b2655296080fc5b485eb1db31a3677e
ganushkevychv <ganushkevychv@gmail.com>
2021-07-20 10:12:57 +0300

    BottomTabBar, TabNavigator was removed from App.tsx and added as additional component

--- App.tsx
+++ App.tsx
@@ -1,31 +1,9 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, BottomNavigation, BottomNavigationTab, IconRegistry } from '@ui-kitten/components';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator, BottomTabBarProps } from '@react-navigation/bottom-tabs';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import DuckScreen from './src/screens/DuckScreen';
-import InfoScreen from './src/screens/InfoScreen';
-import BirdIcon from './src/components/Icons/BirdIcon';
-import InfoIcon from './src/components/Icons/InfoIcon';
-
-const BottomTabBar: React.FC<BottomTabBarProps> = ({ navigation, state }) => (
-  <BottomNavigation
-    selectedIndex={state.index}
-    onSelect={index => navigation.navigate(state.routeNames[index])}>
-    <BottomNavigationTab title='DUCK' icon={BirdIcon}/>
-    <BottomNavigationTab title='INFO' icon={InfoIcon}/>
-  </BottomNavigation>
-);
-
-const { Navigator, Screen } = createBottomTabNavigator();
-
-const TabNavigator = () => (
-  <Navigator tabBar={props => <BottomTabBar {...props} />}>
-    <Screen name='Duck' component={DuckScreen}/>
-    <Screen name='Info' component={InfoScreen}/>
-  </Navigator>
-);
+import TabNavigator from './src/navigation/TabNavigator';
 
 export default function App() {
   return (
